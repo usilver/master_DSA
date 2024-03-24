6f8d5472207e2aa6103c888fe581c223dfee8474
Kundan <kumarkundan0809@icloud.com>
2024-03-25 01:35:40 +0530

    Binary search in an ordered array

---
+++ Array/src/09-binary_search_algorithm.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Binary_Search_Algorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter size of an array: ");
+        int n = sc.nextInt(); // array size
+        System.out.print("Please enter your target: ");
+        int tar = sc.nextInt(); // target
+        int[] arr =new int[n] ;// creating array of n size
+
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        int search = binarySearch(arr, n, tar);
+        if (search < 0){
+            System.out.print("Your target does not exist!");
+        }else {
+            System.out.print("Your target is found at index: " + search);
+        }
+    }
+    static int binarySearch(int[] arr, int n,  int tar){
+        int lo = 0; // lowest
+        int hi = n - 1; // highest
+
+        while (lo <= hi){
+            int mid = (lo + hi)/2;
+            if (arr[mid] == tar){
+                return mid;
+            }else if(arr[mid] < tar) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return -1; // if element is not found
+    }
+}
