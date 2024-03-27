f348e92c31193a5c933d17a6de8f7fbb6dc3782a
Kundan <kumarkundan0809@icloud.com>
2024-03-28 00:34:57 +0530

    Brute Force | O(n^2)

---
+++ Array/src/11-max_subArray_k_length.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Max_SubArray_K_Length {
+    static int maxSubArray(int[] arr, int k){
+        int max_sum = Integer.MIN_VALUE;
+        for (int sp = 0; sp < arr.length - 1 - k; sp++){
+            int c_sum = 0;
+            for (int ep = sp; ep <= sp + k - 1; ep++){
+                c_sum = c_sum + arr[ep];
+            }
+            System.out.println(c_sum);
+            if (max_sum < c_sum){
+                max_sum = c_sum;
+            };
+        }
+        return max_sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the size of an array: ");
+        int n = sc.nextInt(); // size of array
+        int[] arr = new int[n]; // array
+        System.out.print("Please enter the size of a subArray: ");
+        int k = sc.nextInt(); // size of subArray
+
+        // array elements
+        for(int i = 0; i < n; i++){
+            System.out.print("Please enter integer " + (i + 1) + ":");
+            arr[i] = sc.nextInt();
+        }
+//        int[] arr = {1, 5, 8, 9, -4, 5, -12, 8, 20};
+//        int[] arr = {-1, -5, -8, -9, -15, -1, -22, -34, -55};
+//        int k = 4;
+
+        int max_subArray = maxSubArray(arr, k);
+        System.out.println(max_subArray);
+    }
+}
