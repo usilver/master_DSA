c0c2d4422cc00fdddc1fc5e17dcc926c8a4dfb37
Kundan <kumarkundan0809@icloud.com>
2024-03-29 17:09:10 +0530

    Brute Force | O(n*n) |

--- Array/src/11-max_subarray_k_length.java
+++ Array/src/11-max_subarray_k_length.java
@@ -1,18 +1,19 @@
-/* Brute Force | O(n^2) | */
 import java.util.Scanner;
-
 class Max_SubArray_K_Length {
     static int maxSubArray(int[] arr, int k){
         int max_sum = Integer.MIN_VALUE;
         for (int sp = 0; sp < arr.length - 1 - k; sp++){
             int c_sum = 0;
+            
             for (int ep = sp; ep <= sp + k - 1; ep++){
                 c_sum = c_sum + arr[ep];
             }
+
             System.out.println(c_sum);
+
             if (max_sum < c_sum){
                 max_sum = c_sum;
-            };
+            }
         }
         return max_sum;
     }
@@ -29,6 +30,8 @@ class Max_SubArray_K_Length {
             System.out.print("Please enter integer " + (i + 1) + ":");
             arr[i] = sc.nextInt();
         }
+        sc.close();
+
 //        int[] arr = {1, 5, 8, 9, -4, 5, -12, 8, 20};
 //        int[] arr = {-1, -5, -8, -9, -15, -1, -22, -34, -55};
 //        int k = 4;
@@ -37,3 +40,7 @@ class Max_SubArray_K_Length {
         System.out.println(max_subArray);
     }
 }
+
+/*
+Brute Force | O(n*n) |
+*/
