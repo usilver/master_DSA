79993a3feeb725c9b66232b5d5ded0c49d3853ae
Kundan <kumarkundan0809@icloud.com>
2024-03-29 17:19:12 +0530

    Binary Search Algo | O(n) |

--- Array/src/09-binary_search_algorithm.java
+++ Array/src/09-binary_search_algorithm.java
@@ -1,7 +1,25 @@
-/*Binary Search Algo | O(n) |*/
 import java.util.*;
 
 class Binary_Search_Algorithm {
+
+    static int binarySearch(int[] arr, int n,  int tar){
+        int lo = 0; // lowest
+        int hi = n - 1; // highest
+
+        while (lo <= hi){
+            int mid = (lo + hi)/2;
+
+            if (arr[mid] == tar){
+                return mid;
+            }else if(arr[mid] < tar) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return -1; // if element is not found
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,27 +33,17 @@ class Binary_Search_Algorithm {
             arr[i] = sc.nextInt();
         }
         sc.close();
+
         int search = binarySearch(arr, n, tar);
+
         if (search < 0){
             System.out.print("Your target does not exist!");
         }else {
             System.out.print("Your target is found at index: " + search);
         }
     }
-    static int binarySearch(int[] arr, int n,  int tar){
-        int lo = 0; // lowest
-        int hi = n - 1; // highest
-
-        while (lo <= hi){
-            int mid = (lo + hi)/2;
-            if (arr[mid] == tar){
-                return mid;
-            }else if(arr[mid] < tar) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-        return -1; // if element is not found
-    }
 }
+
+/*
+Binary Search Algo | O(n) |
+*/
