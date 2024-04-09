a1a724032af74e1b6ed347cdaef2867f8c8753cc
Kundan <kumarkundan0809@icloud.com>
2024-04-10 04:51:35 +0530

    Binary Search Algo | O(n) |

--- 02-Array/src/09-binary_search_algorithm.java
+++ 02-Array/src/09-binary_search_algorithm.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Binary_Search_Algorithm {
 
     static int binarySearch(int[] arr, int n,  int tar){
+
         int lo = 0; // lowest
         int hi = n - 1; // highest
 
@@ -21,12 +22,15 @@ class Binary_Search_Algorithm {
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Please enter size of an array: ");
         int n = sc.nextInt(); // array size
+
         System.out.print("Please enter your target: ");
         int tar = sc.nextInt(); // target
+
         int[] arr =new int[n] ;// creating array of n size
 
         for (int i = 0; i < n; i++){
