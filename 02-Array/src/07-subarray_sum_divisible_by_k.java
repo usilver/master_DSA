be77ebbdd77107c89e8a5baf5ae8a73a2f0d5002
Kundan <kumarkundan0809@icloud.com>
2024-04-10 04:45:56 +0530

    Brute Force | O(n*n*n) |

--- 02-Array/src/07-subarray_sum_divisible_by_k.java
+++ 02-Array/src/07-subarray_sum_divisible_by_k.java
@@ -3,9 +3,11 @@ import java.util.*;
 class SubArray_Sum_Divisible_By_k {
 
     static int subArrayDivisibleByK(int[] arr, int n, int k){
+
         int ans = 0;
         for(int sp = 0; sp < n ; sp++){
             for(int ep = sp; ep < n; ep++){
+
                 //subArray from sp (starting point) to ep (end point)
                 // check if array from sp to ep is divisible by k
                 boolean isDivisible = subArraySumDivisibleByK(arr, sp, ep, k);
@@ -17,6 +19,7 @@ class SubArray_Sum_Divisible_By_k {
         }
         return ans;
     }
+
     static boolean subArraySumDivisibleByK(int[] arr, int sp, int ep, int k){
         int sum = 0;
 
@@ -27,15 +30,21 @@ class SubArray_Sum_Divisible_By_k {
         }
         return (sum % k == 0);
     }
+
     public static void main(String[] args) {
+
         Scanner sc=new Scanner(System.in);
+
         int n=sc.nextInt();
         int k=sc.nextInt();
         int[] arr =new int[n];
+
         for(int i=0;i<n;++i){
             arr[i]=sc.nextInt();
         }
+
         int result = subArrayDivisibleByK(arr, n, k);
+
         System.out.print(result);
         System.out.println('\n');
     }
