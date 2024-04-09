1e125d1537d319f926027bd455a42ec81073b245
Kundan <kumarkundan0809@icloud.com>
2024-04-10 04:42:27 +0530

    Rotate An Array

--- 02-Array/src/06-rotate_array.java
+++ 02-Array/src/06-rotate_array.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 
-/*
-Given an array with N elements, the task is to rotate the array to the right by K steps.
-Note : if K is negative , rotate the array to the left.
-* */
-
 class Rotate_Array {
     static void rotateArray(int[] nums, int k) {
+
         int n = nums.length;
         k = k % n;
 
@@ -20,6 +16,7 @@ class Rotate_Array {
     }
 
     static void reverse(int[] nums, int si, int ei){
+
         while(si <= ei){
             int temp = nums[si];
             nums[si] = nums[ei];
@@ -31,6 +28,7 @@ class Rotate_Array {
 
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
@@ -50,4 +48,9 @@ class Rotate_Array {
         scanner.close();
     }
 }
+
+/*
+Given an array with N elements, the task is to rotate the array to the right by K steps.
+Note : if K is negative , rotate the array to the left.
+*/
 
