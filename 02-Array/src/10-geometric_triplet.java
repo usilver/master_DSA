36bb51b28ee13005a9eac4aefb7dcc7b28d8ce0e
Kundan <kumarkundan0809@icloud.com>
2024-04-10 04:54:02 +0530

    Brute Force | O(n*n*n) |

--- 02-Array/src/10-geometric_triplet.java
+++ 02-Array/src/10-geometric_triplet.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class Geometric_Triplet {
     public static void findGeometricTriplets(int[] arr, int n) {
+
         // logic: [2, 4, 8] 4*4 == 2*8
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
@@ -15,12 +16,16 @@ class Geometric_Triplet {
     }
 
     public static void main(String[] args) throws Exception {
+
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         int[] arr = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
+
         findGeometricTriplets(arr, N);
         sc.close();
     }
