43ccc940f3178277c97c0989d9b3b729b1561585
Kundan <kumarkundan0809@icloud.com>
2024-04-15 01:59:53 +0530

    Level: Easy | TC: O(n*n) |

--- 03-Array_2D/src/01-print_2d_array.java
+++ 03-Array_2D/src/01-print_2d_array.java
@@ -18,12 +18,19 @@ class Array2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n, m;
+        System.out.print("Please enter length of row: ");
         n = sc.nextInt();
+        System.out.print("Please enter length of column: ");
         m = sc.nextInt();
         int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("[" + i + "][" + j + "] : ");
                 arr[i][j] = sc.nextInt();
+            }
+        }
+        
         Print2DArray Obj = new Print2DArray();
         Obj.printElements(arr);
         sc.close();
