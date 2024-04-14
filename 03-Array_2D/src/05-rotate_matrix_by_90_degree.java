9df2287a2991705e7caf6cd2eec91278f1c0ec50
Kundan <kumarkundan0809@icloud.com>
2024-04-15 02:12:31 +0530

    Level: Medium | TC: O(n*n) |

--- 03-Array_2D/src/05-rotate_matrix_by_90_degree.java
+++ 03-Array_2D/src/05-rotate_matrix_by_90_degree.java
@@ -86,4 +86,51 @@ class Rotate_Matrix_By_90_Degree {
     }
 }
 
-// Level: Medium
+/*
+Level: Medium | Time Complexity: O(n*n) |
+
+Rotate a Matrix by 90 Degree
+
+You are given a (n x n) 2-D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+Input Format
+The first line will contain two integers n and n again.
+
+The next n lines contain n integers each representing the matrix.
+
+Output Format
+Print a matrix that is rotated by 90 degrees in the clockwise direction.
+
+Example 1
+Input
+
+2 2
+1 2
+2 3
+Output
+
+2 1
+3 2
+Explanation
+
+We do a 90-degree clockwise rotation of the matrix.
+
+Example 2
+Input
+
+3 3
+7  2  3
+2  3  4
+5  6  1
+Output
+
+5 2 7
+6 3 2
+1 4 3
+Explanation
+
+We do a 90-degree clockwise rotation of the matrix.
+
+Constraints
+1 <= n <= 100
+*/
